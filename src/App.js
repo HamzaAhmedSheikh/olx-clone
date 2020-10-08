b64f0fb7f3167f647abf7f81c6ba0a8ca9b175e9
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-10-09 03:38:16 +0500

    navbar and sub-navbar is complete.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-// import './Props.css';
+import './Props.css';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 // import HomePage from './components/Home'
 // import Header from './components/Header'
 // import Footer from './components/Footer'
-import Navbar from './components/component/Navbar'
+import Navbars from './components/component/Navbar'
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Navbars />
       {/* <Route path="/" component={HomePage} />       */}
       {/* <Footer /> */}      
     </Router>    
