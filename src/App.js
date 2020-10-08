6ff0044787be607b773d83ab3a378e192e34fcec
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-10-09 03:53:32 +0500

    add banner

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 // import HomePage from './components/Home'
 // import Header from './components/Header'
 // import Footer from './components/Footer'
-import Navbars from './components/component/Navbar'
+import Navbars from './components/component/Navbar';
+import Banner from './components/component/Banner';
 
 function App() {
   return (
     <Router>
       <Navbars />
+      <Banner />
       {/* <Route path="/" component={HomePage} />       */}
       {/* <Footer /> */}      
     </Router>    
---
+++ src/components/component/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Jumbotron, Container } from 'react-bootstrap';
+import BGImage from '../../images/hero_bg.jpg'
+
+ function Banner () {
+    return(
+      <section>
+        <div className="_2Fcb3">
+          <div class="_2gCSl _3TqoF">
+              as
+          </div>   
+        </div>  
+      </section>     
+    )
+ }
+
+ export default Banner;
+
