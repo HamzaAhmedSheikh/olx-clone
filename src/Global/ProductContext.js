957a5a7fc447bc5d455b199ba93e0daeaa44f93e
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-10-10 03:21:54 +0500

    add product images.

---
+++ src/Global/ProductContext.js
@@ -0,0 +1,6 @@
+import React, { createContext, useState } from 'react';
+
+import wirelessheadphone from '../images/wireless-headphones.jpg';
+import cocacola from '../images/coca-cola bottle.jpg';
+import table from '../images/table.jpg';
+import sofa from '../images/2-seater-sofa.jpg';
