e910942f3f70ba5edac490222e88a1254a84bd62
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-10-17 22:53:05 +0500

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,16 @@
+importScripts("https://www.gstatic.com/firebasejs/7.24.0/firebase-app.js");
+ 
+  const firebaseConfig = {
+    apiKey: "AIzaSyD8SlSK0y9E70rJExfZgtfSwoNRSINYINc",
+    authDomain: "olx-clone-a4154.firebaseapp.com",
+    databaseURL: "https://olx-clone-a4154.firebaseio.com",
+    projectId: "olx-clone-a4154",
+    storageBucket: "olx-clone-a4154.appspot.com",
+    messagingSenderId: "415000918559",
+    appId: "1:415000918559:web:39847c548c7e941b77732a",
+    measurementId: "G-NVM0CLQM2Q"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
+
