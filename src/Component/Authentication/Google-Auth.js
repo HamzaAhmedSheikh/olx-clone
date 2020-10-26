3977077d8ea7457ff5091e0ead042d4d9b6b680a
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-10-26 22:55:58 +0500

    Update Google-Auth.js

--- src/Component/Authentication/Google-Auth.js
+++ src/Component/Authentication/Google-Auth.js
@@ -1,32 +1,32 @@
 import { auth, google_provider } from '../../Pages/Login';
 
-export const Google_Auth_Login = (dispatch) => {
+ export const Google_auth_login = (dispatch) => {
 
-   auth().signInWithPopup(google_provider).then(({ user }) => {
-
-    dispatch({
+    auth().signInWithPopup(google_provider).then(({ user }) => {
+                
+     dispatch({
         type: "Set_User_Data",
         username: user.displayName,
         photourl: user.photoURL,
         user: true,
         email: user.email
-     })
-   })
- } 
+      })
+    })
+  }
 
-export const  Google_Auth_Logout = (dispatch) => {
+ export const Google_auth_logout = (dispatch) => {
 
-  auth().signOut().then(function() {  
+    auth().signOut().then(function() {
 
-    dispatch({
+     dispatch({
         type: "Set_User_Data",
         username: null,
         photourl: null,
         user: false, 
-        email: null 
+        email: null
+     })
     }).catch(function(error) {
-        console.log(error)
-    });
+       console.log(error)
+   });
  }
-
 
