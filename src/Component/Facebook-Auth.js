181e2fbdcdb898976276f45e5c5c0c015fb3659c
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-10-26 22:54:02 +0500

    firebase facebook auth.

--- src/Component/Facebook-Auth.js
+++ src/Component/Facebook-Auth.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { auth, provider } from '../../Pages/Login'
 
-export const Facebook-Auth = () => {
-    return (
-        <div>
-           hello world 
-        </div>
-    )
+
+ export const Fb_auth_login = (dispatch) => {
+
+   auth().signInWithPopup(provider).then(({ user }) => {
+    
+    dispatch({
+        type: "Set_User_Data",
+        username: user.displayName,
+        photourl: user.photoURL,
+        user: true,
+         email: user.email
+    })
+  })
 }
+
+ export const Fb_auth_logout = (dispatch) => {
+
+    auth().signOut().then(function() {
+
+     dispatch({
+        type: "Set_User_Data",
+        username: null,
+        photourl: null,
+        user: false, 
+        email: null
+     })
+  }).catch(function(error) {
+      console.log(error)
+  });
+}
+
+
