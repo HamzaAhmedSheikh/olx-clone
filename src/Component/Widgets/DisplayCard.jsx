0fc1adbddff35087331f006cba6c9392e1b50afc
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-10-28 02:32:45 +0500

    update DisplayCards.

--- src/Component/Widgets/DisplayCard.jsx
+++ src/Component/Widgets/DisplayCard.jsx
@@ -1,104 +1,116 @@
-import React from 'react';
-import { Card, CardActionArea, CardActions, CardContent, CardMedia, Typography, IconButton } from '@material-ui/core';
-import * as materialIcon from "@material-ui/icons"; //materialIcon.Search
+import React from 'react'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import DetailsIcon from '@material-ui/icons/Details';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import { makeStyles } from '@material-ui/core/styles';
-import {Link} from 'react-router-dom'
-import { useStateValue } from '../../GlobalState/ContextProvider'; 
+import IconButton from '@material-ui/core/IconButton';
+import {Link} from 'react-router-dom';
+import {useStateValue} from '../../GlobalState/ContextProvider';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
- const useStyles = makeStyles({
-    root: {
-        maxWidth: 345,
-        minWidth: 260,
-        display: 'inline-block',
-        margin: '20px 10px',
-        textAlign: 'left'
-    },
-
-    media: {
-        textAlign: 'center', 
-    },
-    
-    price: {
-        fontSize: '25px',
-        textAlign: 'left',
-        fontWeight: '700'
-    },
-
-    title: {
-        fontSize: '14px',
-        textAlign: 'left',
-        fontWeight: '500',
-        height: '20px',
-        overflow: 'hidden'
-    },
-
-    desc: {
-        fontSize: '14px',
-        textAlign: 'left',
-        fontWeight: '400',
-        height: '22px',
-        overflow: 'hidden'
-    }
-
- });
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    minWidth: 260,
+    display: 'inline-block',
+    margin: '20px 10px',
+    textAlign: 'left',
+    border: '1px solid black'
+  },
+  media: {
+    textAlign: 'center',
 
 
- const DisplayCard = (props) => {
-   const classes = useStyles();
-   const [{basket}, dispatch] = useStateValue();
+  },
+  price: {
+    fontSize: '25px',
+    textAlign: 'left',
+    fontWeight: '700'
+  },
+  title: {
+    fontSize: '14px',
+    textAlign: 'left',
+    fontWeight: '500',
+    height: '20px',
+    overflow: 'hidden'
+  },
+  desc: {
+    fontSize: '14px',
+    textAlign: 'left',
+    fontWeight: '400',
+    height: '22px',
+    overflow: 'hidden'
+  },
+  heart: {
+    display: 'flex',
+    flexDirection: 'row',
+    textAlign: "right"
+  }
+});
 
-   const ToggleFavourite = (prop) => {
-    (basket.filter(({id})=> {
+const DisplayCard = (props) => {
+
+    const classes = useStyles();
+    const [{basket}, dispatch] = useStateValue();
+    const ToggleFavourite = (prop) => {
+      (basket.filter(({id})=> {
         return +id === +props.id
-    })).length === 0 ?
-       dispatch({
+      } 
+        )).length === 0 ? 
+      dispatch({
         type: 'Add_To_Basket',
         item: prop
-    }): 
+      }): 
       dispatch({
-          type: "Remove_From_Basket",
-          item: prop,
-    })
-   }
+        type: "Remove_From_Basket",
+        item: prop,
+      })
+    }
 
     return (
-     <Card className={classes.root}>  
-       <CardActions >
-        <IconButton aria-label="add to favorites" style={{border: 'none', outline: 'none'}} onClick={()=>ToggleFavourite(props)}>
-           {(basket.filter(({id})=> {
-              return +id === +props.id
-            })).length > 0 ? <materialIcon.FavoriteIcon  style={{border: 'none', outline: 'none'}} />
-            : <materialIcon.FavoriteBorderIcon  style={{border: 'none', outline: 'none'}} />
-           }            
-        </IconButton>
+        <Card className={classes.root}>
+          
+          <CardActions >
+          <IconButton aria-label="add to favorites" style={{border: 'none', outline: 'none'}} onClick={()=>ToggleFavourite(props)}>
+          {(basket.filter(({id})=> {
+            return +id === +props.id
+          }
+            )).length > 0 ? <FavoriteIcon  style={{border: 'none', outline: 'none'}}/>
+           : <FavoriteBorderIcon className={classes.heart} style={{border: 'none', outline: 'none'}}/>
+        }
+            
+          </IconButton>
+          <IconButton aria-label="add to favorites" style={{border: 'none', outline: 'none'}}>
+            <DetailsIcon  style={{border: 'none', outline: 'none'}}/>
+          </IconButton>
+         </CardActions>
+          <CardActionArea  component={Link} to={`/add/${props.id}`}  style={{textDecoration: 'none', color: 'black'}}>
+          <CardMedia
+            className={classes.media}
+          >
+          <img src={props.img} alt={props.title} className='img' style ={{width: '280px', height: '200px', minWidth: '100px'}}/>
+          </CardMedia>
 
-        <IconButton aria-label="add to favorites" style={{border: 'none', outline: 'none'}}>
-           <materialIcon.DetailsIcon style={{border: 'none', outline: 'none'}} />
-        </IconButton>
-       </CardActions>
-
-       <CardActionArea component={Link} to={`/add/${props.id}`}  style={{textDecoration: 'none', color: 'black'}}>
-        <CardMedia className={classes.media}>
-           <img src={props.img} alt={props.title} className='img' style ={{width: '280px', height: '200px', minWidth: '100px'}} />
-        </CardMedia>
-
-        <CardContent>
+          <CardContent>
           <Typography gutterBottom variant="p" component="p" className={classes.price}>
-             Rs. {props.price* 100}
+            Rs. {props.price* 100}
           </Typography>
-
           <Typography gutterBottom variant="p" component="p" className={classes.title}>
-             {props.title}
+            {props.title}
           </Typography>
-
           <Typography variant="body2" color="textSecondary" component="p" className={classes.desc}>
-             {props.description+ '....'}
+            {props.description+ '....'}
           </Typography>
-        </CardContent>
+          </CardContent>
+          </CardActionArea>
+         
+      </Card>
+    )
+}
 
-       </CardActionArea>
-     </Card>
-   )
- }
-
-  export default DisplayCard ;
+export default DisplayCard;
--- src/Component/Widgets/DisplayCard2.jsx
+++ src/Component/Widgets/DisplayCard2.jsx
@@ -1,111 +1,115 @@
-import React from 'react';
-import { Card, CardActionArea, CardActions, CardContent, CardMedia, Typography, IconButton } from '@material-ui/core';
-import * as materialIcon from "@material-ui/icons"; //materialIcon.Search
+import React from 'react'
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import DetailsIcon from '@material-ui/icons/Details';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import { makeStyles } from '@material-ui/core/styles';
-import { useStateValue } from '../../GlobalState/ContextProvider';
-import { firebase_storage } from '../../Pages/Login_Auth';
-import {Link} from 'react-router-dom'
+import IconButton from '@material-ui/core/IconButton';
+import {useStateValue} from '../../GlobalState/ContextProvider' 
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import {Link} from 'react-router-dom';
 
- const useStyles = makeStyles({
-    root: {
-        maxWidth: 345,
-        minWidth: 260,
-        display: 'inline-block',
-        margin: '20px 10px',
-        textAlign: 'left'
-    },
-    
-    media: {
-       textAlign: 'center',
-    },
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    minWidth: 260,
+    display: 'inline-block',
+    margin: '20px 10px',
+    textAlign: 'left'
+  },
+  media: {
+    textAlign: 'center',
 
-    price: {
-        fontSize: '25px',
-        textAlign: 'left',
-        fontWeight: '700'
-    },
 
-    title: {
-        fontSize: '14px',
-        textAlign: 'left',
-        fontWeight: '500',
-        height: '20px',
-        overflow: 'hidden'
-    },
+  },
+  price: {
+    fontSize: '25px',
+    textAlign: 'left',
+    fontWeight: '700'
+  },
+  title: {
+    fontSize: '14px',
+    textAlign: 'left',
+    fontWeight: '500',
+    height: '20px',
+    overflow: 'hidden'
+  },
+  desc: {
+    fontSize: '14px',
+    textAlign: 'left',
+    fontWeight: '400',
+    height: '22px',
+    overflow: 'hidden'
+  }
+});
 
-    desc: {
-        fontSize: '14px',
-        textAlign: 'left',
-        fontWeight: '400',
-        height: '22px',
-        overflow: 'hidden'
-    }
- });
-
- const DisplayCard2 = (props) => {
+const DisplayCard2 = (props) => {
 
     const classes = useStyles();
     const [{basket}, dispatch] = useStateValue();
-
     const ToggleFavourite = (prop) => {
-       (basket.filter(({id})=> {
-          return +id === +props.id
-        })).length === 0 ? 
-          dispatch({
-            type: 'Add_To_Basket',
-            item: prop
-        }): 
-          dispatch({
-            type: "Remove_From_Basket",
-            item: prop,
-        })
+      (basket.filter(({id})=> {
+        return +id === +props.id
+      } 
+        )).length === 0 ? 
+      dispatch({
+        type: 'Add_To_Basket',
+        item: prop
+      }): 
+      dispatch({
+        type: "Remove_From_Basket",
+        item: prop,
+      })
     }
+    console.log(props.id)
 
-      console.log(props.id);
-      
-   return (
-    <Card className={classes.root}>
-     <CardActions>
-      <IconButton aria-label="add to favorites" style={{border: 'none', outline: 'none'}} onClick={()=>ToggleFavourite(props)}>
-        {(basket.filter(({id})=> {
+    return (
+        <Card className={classes.root}>
+
+          <CardActions >
+          <IconButton aria-label="add to favorites" style={{border: 'none', outline: 'none'}} onClick={()=>ToggleFavourite(props)}>
+          {(basket.filter(({id})=> {
             return +id === +props.id
-        })).length > 0 ? <materialIcon.FavoriteIcon style={{border: 'none', outline: 'none'}} />
-          : <materialIcon.FavoriteBorderIcon style={{border: 'none', outline: 'none'}} />
+          }
+            )).length > 0 ? <FavoriteIcon  style={{border: 'none', outline: 'none'}}/>
+          : <FavoriteBorderIcon  style={{border: 'none', outline: 'none'}}/>
         }
               
-      </IconButton>
-
-       <IconButton aria-label="add to favorites" style={{border: 'none', outline: 'none'}}>
-         <materialIcon.DetailsIcon style={{border: 'none', outline: 'none'}} />
-       </IconButton>
-     </CardActions>      
-
-      <CardActionArea component={Link} to={`/add_users/${props.id.toString()}`} style={{textDecoration: 'none', color: 'black'}}>
-        <CardMedia className={classes.media}>
+          </IconButton>
+          <IconButton aria-label="add to favorites" style={{border: 'none', outline: 'none'}}>
+            <DetailsIcon  style={{border: 'none', outline: 'none'}}/>
+          </IconButton>
+         </CardActions>
+          <CardActionArea component={Link} to={`/add_users/${props.id.toString()}`}  style={{textDecoration: 'none', color: 'black'}}>
+          <CardMedia
+            className={classes.media}
+          >
           <img 
-            src={`https://firebasestorage.googleapis.com/v0/b/pwa-olx-clone-application.appspot.com/o/${props.id.toString()}?alt=media&token=4dede770-68f6-4c7e-879f-da97e126463a`}
-            alt={props.title}
-            className='img'
-            style ={{width: '280px', height: '200px', minWidth: '100px'}}
+          src={`https://firebasestorage.googleapis.com/v0/b/pwa-olx-clone-application.appspot.com/o/${props.id.toString()}?alt=media&token=4dede770-68f6-4c7e-879f-da97e126463a`}
+          alt={props.title}
+          className='img'
+          style ={{width: '280px', height: '200px', minWidth: '100px'}}
           />
-        </CardMedia>
-
-        <CardContent>
+          </CardMedia>
+          <CardContent>
           <Typography gutterBottom variant="p" component="p" className={classes.price}>
             Rs. {props.price}
           </Typography>
-
           <Typography gutterBottom variant="p" component="p" className={classes.title}>
             {props.title}
           </Typography>
-
           <Typography variant="body2" color="textSecondary" component="p" className={classes.desc}>
             {props.description+ '....'}
-          </Typography> 
-        </CardContent>
-      </CardActionArea>
-    </Card>  
-   )    
- }
- 
- export default DisplayCard2;
+          </Typography>
+          </CardContent>
+          </CardActionArea>
+         
+      </Card>
+    )
+}
+
+export default DisplayCard2;
